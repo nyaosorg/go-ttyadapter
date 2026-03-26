53ebf42ef47465ec67f6a160455231b492946c50
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-03-26 16:16:01 +0900

    `tty10pe.Tty`: Add field `OnPrefix` and method `SetOnPrefix`

--- tty10pe/example.go
+++ tty10pe/example.go
@@ -11,8 +11,12 @@ import (
 	"github.com/nyaosorg/go-ttyadapter/internal/demo"
 )
 
+func onPrefix(s string) {
+	fmt.Printf("onPrefix: %q\r\n", s)
+}
+
 func main() {
-	if err := demo.Exec(&tty10pe.Tty{}); err != nil {
+	if err := demo.Exec(&tty10pe.Tty{OnPrefix: onPrefix}); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
 	}
