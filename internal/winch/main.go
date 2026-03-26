60fd3cf509e15daf27a66cbde7def10202f66e93
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-03-26 12:43:37 +0900

    "internal/winch": Use "golang.org/x/sys/unix".SIGWINCH on UNIX like systems

--- internal/winch/main.go
+++ internal/winch/main.go
@@ -1,10 +1,7 @@
 package winch
 
 import (
-	"context"
 	"os"
-	"sync"
-	"time"
 
 	"golang.org/x/term"
 )
@@ -14,34 +11,5 @@ func getSize() (int, int, error) {
 }
 
 func Notice(onResize func(int, int)) (func(), error) {
-	w, h, err := getSize()
-	if err != nil {
-		return nil, err
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func(lastw, lasth int) {
-		ticker := time.NewTicker(time.Second)
-		for {
-			select {
-			case <-ctx.Done():
-				ticker.Stop()
-				wg.Done()
-				return
-			case <-ticker.C:
-				w, h, err := getSize()
-				if err == nil && (w != lastw || h != lasth) {
-					onResize(w, h)
-					lastw = w
-					lasth = h
-				}
-			}
-		}
-	}(w, h)
-
-	return func() {
-		cancel()
-		wg.Wait()
-	}, nil
+	return notice(onResize)
 }
