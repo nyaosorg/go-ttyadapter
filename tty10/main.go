50555dea7569e0d028601c67f83532e419fdd2a9
HAYAMA Kaoru <3752189+hymkor@users.noreply.github.com>
2026-05-10 02:37:44 +0900

    tty10: Improve Open/Close state management

--- tty10/main.go
+++ tty10/main.go
@@ -20,24 +20,32 @@ func (tt *Tty) fd() int {
 	return int(tt.devTty.Fd())
 }
 
-func (tt *Tty) Open(onResize func(int, int)) error {
-	var err error
+func (tt *Tty) Open(onResize func(int, int)) (err error) {
+	defer func() {
+		if err != nil {
+			tt.Close()
+		}
+	}()
 
-	tt.devTty, err = os.OpenFile(tty10base.TtyPath, os.O_RDWR, 0666)
-	if err != nil {
-		return err
+	if tt.devTty == nil {
+		tt.devTty, err = os.OpenFile(tty10base.TtyPath, os.O_RDWR, 0666)
+		if err != nil {
+			return
+		}
 	}
 
-	tt.disable, err = tty10base.EnableVirtualTerminal(tt.fd())
-	if err != nil {
-		return err
+	if tt.disable == nil {
+		tt.disable, err = tty10base.EnableVirtualTerminal(tt.fd())
+		if err != nil {
+			return
+		}
 	}
 
 	if onResize != nil {
 		tt.cancel, err = tty10base.Notice(onResize)
-		return err
+		return
 	}
-	return nil
+	return
 }
 
 func (tt *Tty) GetKey() (key string, err error) {
@@ -67,6 +75,7 @@ func (tt *Tty) Close() error {
 	}
 	if tt.devTty != nil {
 		tt.devTty.Close()
+		tt.devTty = nil
 	}
 	return nil
 }
